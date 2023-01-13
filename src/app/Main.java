fd8c3c2536c3bccfa9f83a17664e23171b7cd7c8
yevhen <grudi4ek@gmail.com>
2023-01-13 19:21:40 +0200

    Fix typo

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,20 +1,25 @@
 package app;
 
 public class Main {
-    int a, b, c, d, e, f;
+    private static int a;
+    private static int b;
+    private static int c;
+    private static int d;
+    private static int e;
+    private static int f;
 
     public static void main(String[] args) {
         System.out.println("My app for Users!");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
-        int c = add(a, b);
+        a = 8;
+        b = 2;
+        c = add(a, b);
         System.out.println(c);
-        int d = multiply(a, b);
+        d = multiply(a, b);
         System.out.println(d);
-        int e = sub(a, b);
+        e = sub(a, b);
         System.out.println(e);
-        int f = div(a, b);
+        f = div(a, b);
         System.out.println(f);
     }
 
